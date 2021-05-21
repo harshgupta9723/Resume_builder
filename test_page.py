036c522be950e1c86c8152fdeb03033f6b4a2d94
Harsh gupta <Harshg9723@gmail.com>
2021-05-22 00:29:08 +0530

    fixed api issues

--- test_page.py
+++ test_page.py
@@ -1,13 +1,20 @@
-from flask import Flask, request, render_template
+from flask import Flask, request
 
 app = Flask(__name__)
+@app.route('/', methods=['GET','POST'])
+def job():
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    gender = request.form.get('gender')
+    birthday = request.form.get('birthday')
+    pincode = request.form.get('pincode')
+    address = request.form.get('address')
+    contact = request.form.get('contact_number')
+    email_id = request.form.get('email_id')
 
-@app.route('/')
-def my_form():
-    return render_template('index.html')
+    # print(firstname, lastname, gender, birthday, pincode, address, contact, email_id)
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+    return "hi"
+
+if __name__ == "__main__":
+    app.run(port=5030)
