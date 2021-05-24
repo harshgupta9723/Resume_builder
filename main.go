cfc309fbac1f915713e4d50d824e0e96ece00c2e
Harsh gupta <Harshg9723@gmail.com>
2021-05-25 01:04:10 +0530

    add golang code in api and model

--- main.go
+++ main.go
@@ -6,11 +6,42 @@ import (
 	"net/http"
 )
 
+type Resume struct {
+	firstName     string `json:"firstName"`
+	lastName      string `json:"lastName"`
+	gender        string `json:"gender"`
+	birthday      string `json:"birthday"`
+	pincode       string `json:"pincode"`
+	address       string `json:"address"`
+	contactNumber string `json:"contactNumber"`
+	emailId       string `json:"emailId"`
+}
+
+func formValidator(resume Resume) {
+
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 
-	// add post method
-	fmt.Fprintf(w, "Welcome to the HomePage!")
+	var resumeForm []Resume
+
+	r.ParseForm()
 	fmt.Println("Endpoint Hit: homePage")
+
+	for _, value := range r.Form {
+		resumeForm = append(resumeForm, Resume{
+			firstName:     value.firstname,
+			lastName:      value.lastname,
+			gender:        value.gender,
+			address:       value.address,
+			contactNumber: value.contact_number,
+			emailId:       value.email_id,
+			birthday:      value.birthday,
+			pincode:       value.pincode,
+		})
+	}
+	fmt.Println(resumeForm)
+
 }
 
 func handleRequests() {
