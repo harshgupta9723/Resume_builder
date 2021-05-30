8607d9dea4f4ee8b86c022c2312009648bab28e5
Harsh gupta <Harshg9723@gmail.com>
2021-05-30 23:06:43 +0530

    cammel case

--- main.go
+++ main.go
@@ -7,44 +7,44 @@ import (
 )
 
 type Resume struct {
-	firstName     string `json:"firstName"`
-	lastName      string `json:"lastName"`
-	gender        string `json:"gender"`
-	birthday      string `json:"birthday"`
-	pincode       string `json:"pincode"`
-	address       string `json:"address"`
-	contactNumber string `json:"contactNumber"`
-	emailId       string `json:"emailId"`
+	FirstName     string `json:"firstName"`
+	LastName      string `json:"lastName"`
+	Gender        string `json:"gender"`
+	Birthday      string `json:"birthday"`
+	Pincode       string `json:"pincode"`
+	Address       string `json:"address"`
+	ContactNumber string `json:"contactNumber"`
+	EmailId       string `json:"emailId"`
 }
 
-func storeData(resumeData Resume) {
+func StoreData(resumeData Resume) {
 	fmt.Println(resumeData)
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
+func HomePage(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
 	fmt.Println("Endpoint Hit: homePage")
 
-	resumeData := Resume{
-		address:       r.Form["address"][0],
-		birthday:      r.Form["birthday"][0],
-		contactNumber: r.Form["contact_number"][0],
-		emailId:       r.Form["email_id"][0],
-		firstName:     r.Form["firstname"][0],
-		lastName:      r.Form["lastname"][0],
-		pincode:       r.Form["pincode"][0],
-		gender:        r.Form["gender"][0],
+	ResumeData := Resume{
+		Address:       r.Form["address"][0],
+		Birthday:      r.Form["birthday"][0],
+		ContactNumber: r.Form["contact_number"][0],
+		EmailId:       r.Form["email_id"][0],
+		FirstName:     r.Form["firstname"][0],
+		LastName:      r.Form["lastname"][0],
+		Pincode:       r.Form["pincode"][0],
+		Gender:        r.Form["gender"][0],
 	}
 
-	storeData(resumeData)
+	StoreData(ResumeData)
 }
 
-func handleRequests() {
-	http.HandleFunc("/submit_resume", homePage)
+func HandleRequests() {
+	http.HandleFunc("/submit_resume", HomePage)
 	log.Fatal(http.ListenAndServe(":5050", nil))
 }
 
 func main() {
-	handleRequests()
+	HandleRequests()
 }
