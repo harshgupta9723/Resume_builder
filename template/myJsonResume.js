cf483a73433b344eff8265475e03df8218511c21
harshgupta9723 <harshg9723@gmail.com>
2021-09-02 23:58:23 +0530

    javascript completed

--- template/myJsonResume.js
+++ template/myJsonResume.js
@@ -1,21 +1,31 @@
-function jsonResume(){
+var name = document.getElementById("Name").value;
+var phoneNumber = document.getElementById("phone_number").value;
+var email = document.getElementById("email").value;
+var option1 = document.getElementById("option_1").value;
+var option2 = document.getElementById("option_2").value;
+var instruction = document.getElementById("instruction").value;
 
-    var rawResume = document.getElementById("Name").value;
+const obj = {userName: name,
+            phone: phoneNumber,
+            email: email, 
+            option1: option1, 
+            option2: option2, 
+            instruction: instruction};
 
-    var data = new FormData();
-    data.append("myForm", rawResume);
+const myJSON = JSON.stringify(obj);
 
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
+var data = new FormData();
+data.append("myForm", myJSON);
 
-    xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-        console.log(this.responseText);
-    }
-    });
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
 
-    xhr.open("POST", "http://127.0.0.1:5030/resume");
-
-    xhr.send(data);
-
+xhr.addEventListener("readystatechange", function() {
+if(this.readyState == 4) {
+    console.log(this.responseText);
 }
+});
+
+xhr.open("POST", "http://127.0.0.1:5030/resume");
+
+xhr.send(data);
